8cfcabe1aeee337483c43040d14f212c4d334c57
applego <applegorillappa@gmail.com>
2020-12-08 13:21:54 +0900

    First Commit MSG

---
+++ Proj1/src/Sample001.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+
+/**
+ * @author yusuke.tsuneshige_ts
+ *
+ */
+public class Sample001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!");
+
+		// 変数i を表示する処理
+		for(int i=0;i<5;i++) {
+			System.out.println("---");
+			System.out.println(i);
+		}
+	}
+
+}
