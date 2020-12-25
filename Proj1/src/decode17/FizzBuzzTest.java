e261b00a6dc4f50597ea94114ef6a93a8196a9bf
常重 友佑 <yusuke.tsuneshige_ts@boc.bookoff.co.jp>
2020-12-26 00:06:17 +0900

    テストを階層構造にする

--- Proj1/src/decode17/FizzBuzzTest.java
+++ Proj1/src/decode17/FizzBuzzTest.java
@@ -2,39 +2,54 @@ package decode17;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.experimental.runners.Enclosed;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
-class FizzBuzzTest {
+@RunWith(Enclosed.class)
+public class FizzBuzzTest {
 
-	private FizzBuzz fizzbuzz;
+//	public static class 三の倍数の場合{
+//
+//	}
+//	public static class 五の倍数の場合{
+//
+//	}
+//	public static class その他の場合{
+//
+//	}
 
-	@BeforeEach
-	public void 前準備() {
-		// 前準備
-		fizzbuzz = new FizzBuzz();
-	}
+	public static class 既存のテスト{
+		private FizzBuzz fizzbuzz;
 
-	@Test
-	public void _1を渡したら文字列1を返す() throws Exception {
-		// 実行
-		// 検証
-		assertEquals("1", fizzbuzz.convert(1));
-	}
+		@BeforeEach
+		public void 前準備() {
+			// 前準備
+			fizzbuzz = new FizzBuzz();
+		}
 
-	@Test
-	public void _2を渡したら文字列2を返す() throws Exception {
-		assertEquals("2", fizzbuzz.convert(2));
-	}
+		@Test
+		public void _1を渡したら文字列1を返す() throws Exception {
+			// 実行
+			// 検証
+			assertEquals("1", fizzbuzz.convert(1));
+		}
 
-	@Test
-	public void _3を渡したら文字列Fizzを返す() throws Exception {
-		assertEquals("Fizz", fizzbuzz.convert(3));
-	}
+		@Test
+		public void _2を渡したら文字列2を返す() throws Exception {
+			assertEquals("2", fizzbuzz.convert(2));
+		}
 
-	@Test
-	public void _5を渡したら文字列Buzzを返す() throws Exception {
-		assertEquals("Buzz", fizzbuzz.convert(5));
+		@Test
+		public void _3を渡したら文字列Fizzを返す() throws Exception {
+			assertEquals("Fizz", fizzbuzz.convert(3));
+		}
+
+		@Test
+		public void _5を渡したら文字列Buzzを返す() throws Exception {
+			assertEquals("Buzz", fizzbuzz.convert(5));
+		}
 	}
 
 }
