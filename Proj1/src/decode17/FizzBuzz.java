dd3e519dc8a82a96cb54d6d49b8ae098e20e76f4
常重 友佑 <yusuke.tsuneshige_ts@boc.bookoff.co.jp>
2020-12-25 22:01:03 +0900

    Greenになったらリファクタリング（コメント消すのもリファクタリング）

--- Proj1/src/decode17/FizzBuzz.java
+++ Proj1/src/decode17/FizzBuzz.java
@@ -3,8 +3,8 @@ package decode17;
 public class FizzBuzz {
 
 	public String convert(int i) {
-		// TODO 自動生成されたメソッド・スタブ
-		return "1";// 仮実装（テストコードのテスト）
+//		return "1";// 仮実装（テストコードのテスト）
+		return String.valueOf(i);
 	}
 
 }
