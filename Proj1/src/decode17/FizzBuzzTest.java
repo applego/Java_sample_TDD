fb2da5953454e410813c866ceac91aa0bce086f9
常重 友佑 <yusuke.tsuneshige_ts@boc.bookoff.co.jp>
2020-12-25 22:10:59 +0900

    3アウト ローカル変数の抽出（fizzbuzzに右クリックから）

--- Proj1/src/decode17/FizzBuzzTest.java
+++ Proj1/src/decode17/FizzBuzzTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+	private FizzBuzz fizzbuzz;
+
 	@Test
 	public void _1を渡したら文字列1を返す() throws Exception {
-		// 前準備
-		FizzBuzz fizzbuzz = new FizzBuzz();
+		fizzbuzz = new FizzBuzz();
 		// 実行
 		// 検証
 		assertEquals("1", fizzbuzz.convert(1));
