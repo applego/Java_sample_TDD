b7edbcd4f92f4ddd383815b3ccae3a970f027943
常重 友佑 <yusuke.tsuneshige_ts@boc.bookoff.co.jp>
2020-12-25 22:02:37 +0900

    リファクタ　引数名変更

--- Proj1/src/decode17/FizzBuzz.java
+++ Proj1/src/decode17/FizzBuzz.java
@@ -2,9 +2,9 @@ package decode17;
 
 public class FizzBuzz {
 
-	public String convert(int i) {
+	public String convert(int num) {
 //		return "1";// 仮実装（テストコードのテスト）
-		return String.valueOf(i);
+		return String.valueOf(num);
 	}
 
 }
