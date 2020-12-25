18f615a7dbaa714ef9ef5874b79f291af438a5b1
常重 友佑 <yusuke.tsuneshige_ts@boc.bookoff.co.jp>
2020-12-25 21:34:12 +0900

    テスト対象のsrcで右クリック⇒新規⇒その他⇒JUnit FizzBuzzTest追加

---
+++ Proj1/src/decode17/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package decode17;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
