1df1bbd375379db5861bcb9ca4764f9e2d970010
常重 友佑 <yusuke.tsuneshige_ts@boc.bookoff.co.jp>
2020-12-25 21:59:11 +0900

    assertionは1テストメソッドに1つ

--- Proj1/src/decode17/FizzBuzz.java
+++ Proj1/src/decode17/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
 	public String convert(int i) {
 		// TODO 自動生成されたメソッド・スタブ
-		return "1";
+		return "1";// 仮実装（テストコードのテスト）
 	}
 
 }
--- Proj1/src/decode17/FizzBuzzTest.java
+++ Proj1/src/decode17/FizzBuzzTest.java
@@ -15,4 +15,13 @@ class FizzBuzzTest {
 		assertEquals("1", fizzbuzz.convert(1));
 	}
 
+	@Test
+	public void _2を渡したら文字列2を返す() throws Exception {
+		// 前準備
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		// 実行
+		// 検証
+		assertEquals("2", fizzbuzz.convert(2));
+	}
+
 }
